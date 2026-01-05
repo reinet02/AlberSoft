ec28cca9407178e1d04bdfc3fdd61b9f4dbc964c
Tony <anthonyaldazg@gmail.com>
2026-01-05 17:54:06 -0500

    Add files via upload

---
+++ AlberSoft/Program.cs
@@ -0,0 +1,21 @@
+namespace AlberSoft
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.SetHighDpiMode(HighDpiMode.PerMonitorV2);
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+            Application.Run(new fmRegistro());
+        }
+    }
+}
